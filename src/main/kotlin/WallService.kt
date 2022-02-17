0d5693266878f88d8329c3b9aed250ae3798e413
Tomato1937 <student@pisem.net>
2022-02-17 14:51:14 +0700

    add Задание 2

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,3 +1,18 @@
 object WallService{
     private var posts = emptyArray<Post>()
+
+    fun add(post: Post): Post {
+        posts += post.copy(id = posts.size +1)
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for ((index, value) in posts.withIndex()) {
+            if (value.id == post.id) {
+                posts[index] = post.copy(ownerId = value.ownerId, date = value.date)
+                return true
+            }
+        }
+        return false
+    }
 }
