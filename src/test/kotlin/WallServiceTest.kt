3b37c4a2ea44f9cb1a5870f0c83774334a7b73db
Tomato1937 <student@pisem.net>
2022-02-17 16:02:11 +0700

    add Задание 3

---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,55 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+    @Test
+    fun add() {
+        //arrange
+        val original = Post(id = 0)
+        val service = WallService
+        val expected = Post(id = 1)
+
+        //act
+        val result = service.add(original)
+
+        //assert
+        assertEquals(expected, result)
+
+    }
+
+    @Test
+    fun update_exist_id() {
+        //arrange
+        val post1 = Post()
+        val post2 = Post(id = 1)
+        val service = WallService
+        val expected = true
+        service.add(post1)
+
+        //act
+        val result = service.update(post2)
+
+        //assert
+        assertEquals(expected, result)
+
+    }
+
+    @Test
+    fun update_not_exist_id() {
+        //arrange
+        val post1 = Post()
+        val post2 = Post(id = 10)
+        val service = WallService
+        val expected = false
+        service.add(post1)
+
+        //act
+        val result = service.update(post2)
+
+        //assert
+        assertEquals(expected, result)
+
+    }
+}
